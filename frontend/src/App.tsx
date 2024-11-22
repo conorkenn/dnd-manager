d476b8a6eb51f7ba45c824e1e21a1ad2ea10a3d8
Conor Kennedy <conor@conorkennedy.com>
2024-11-22 18:45:52 -0500

    small style change - frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,24 +6,24 @@ import CreateCharacterPage from './pages/CreateCharacterPage';
 import ListCharactersPage from './pages/ListCharactersPage';
 import DicePage from './pages/DicePage';
 
-
 const App: React.FC = () => {
   return (
     <Router>
       <nav>
-        <Link to="/"> Home </Link>
-        <Link to="/create-character"> Create </Link>
-        <Link to="/characters"> List </Link>
-        <Link to="/dice"> Dice </Link>
+        <Link to="/">Home</Link>
+        <Link to="/create-character">Create</Link>
+        <Link to="/characters">List</Link>
+        <Link to="/dice">Dice</Link>
       </nav>
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/create-character" element={<CreateCharacterPage/>}/>
-        <Route path="/characters" element={<ListCharactersPage/>}/>
-        <Route path="/dice" element={<DicePage/>}/>
-      </Routes>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create-character" element={<CreateCharacterPage />} />
+          <Route path="/characters" element={<ListCharactersPage />} />
+          <Route path="/dice" element={<DicePage />} />
+        </Routes>
+      </div>
     </Router>
-
   );
 };
 
